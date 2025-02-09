63c0411cb5e394aa6d0aa86c22efaa832ad1fb7b
Serhii K <serhii.kokhan5@gmail.com>
2025-02-09 15:55:38 +0100

    Check connection with PostgreDB

---
+++ app/database.py
@@ -0,0 +1,33 @@
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+## DB connect
+DB_NAME = "mydatabase"
+DB_USER = "myuser"
+DB_PASSWORD = "mypassword"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+
+def get_connection():
+    try:
+        conn = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT,
+        cursor_factory=RealDictCursor
+    )
+        return conn
+    except Exception as e:
+        print(f"Error connection to DB: {e}")
+        return None
+
+# Check connection
+if __name__ == "__main__":
+    conn = get_connection()
+    if conn:
+        print("✅ Successful connected to DB!")
+        conn.close()
+    else:
+        print("❌ Issue during DB connect.")
